45b07b56da3e08bf6d02d414d45f34f6433ed691
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-06-25 10:20:50 +0100

    j'ai cree projet spring boot

---
+++ demo/src/main/java/com/example/demo/DiabetesTrackerAsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesTrackerAsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesTrackerAsApplication.class, args);
+	}
+
+}
