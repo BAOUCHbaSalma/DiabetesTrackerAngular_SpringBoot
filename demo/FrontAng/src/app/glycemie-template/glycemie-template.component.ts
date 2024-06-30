85b663b19794edb7a899e96ba5238b7a6dbd15ca
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-06-30 14:17:40 +0100

    j'ai ajoutee chart js

--- demo/FrontAng/src/app/glycemie-template/glycemie-template.component.ts
+++ demo/FrontAng/src/app/glycemie-template/glycemie-template.component.ts
@@ -1,12 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {MatTableDataSource} from "@angular/material/table";
 import {Observable} from "rxjs";
 
 import {DiabetiqueService} from "../services/diabetique.service";
 import {Glycemie} from "../Models/diabetique.models";
-
-
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-glycemie-template',
@@ -15,9 +14,11 @@ import {Glycemie} from "../Models/diabetique.models";
 })
 export class GlycemieTemplateComponent implements OnInit{
   glycemies: Glycemie[] = [];
+  chart: Chart | undefined;
 
 
   constructor(private diabetiqueService :DiabetiqueService) {
+    Chart.register(...registerables);
 }
 
   ngOnInit(): void {
@@ -25,9 +26,11 @@ export class GlycemieTemplateComponent implements OnInit{
   }
 
 
+
   getGlycemie(){
     this.diabetiqueService.getAllGlycemie().subscribe(value => {
       this.glycemies=value
+      this.chartJs()
   })}
 
   deleteGlycemie(id:number){
@@ -37,6 +40,37 @@ export class GlycemieTemplateComponent implements OnInit{
     )
 
     }
+  chartJs(){
+    const dateElements = this.glycemies.map(g => g.date);
+    const valueElements = this.glycemies.map(g => g.valeurAfter);
+    const ctx = (document.getElementById('chart-1') as HTMLCanvasElement).getContext('2d');
+    if (ctx) {
+      if (this.chart) {
+        this.chart.destroy();
+      }
 
+      this.chart = new Chart(ctx, {
+
+        type: 'line',
+        data: {
+          labels: dateElements,
+          datasets: [{
+            label: 'Valeurs de Glycémie',
+            data: valueElements,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            fill: false
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+  }
 
 }
